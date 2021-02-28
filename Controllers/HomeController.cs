9a9f5c40c560344be2dda9247e8c7d86f65163df
Villanite <ariel@villanea.com>
2021-02-27 20:56:04 -0500

    Adding PoC code to auto-generate Github Issues.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using VillaniteSite.Models;
 
@@ -11,11 +14,15 @@ namespace VillaniteSite.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        HttpClient httpclient = new HttpClient();
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+        private IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public IActionResult Index()
@@ -33,6 +40,19 @@ namespace VillaniteSite.Controllers
 
         public IActionResult Privacy()
         {
+            string gitHubApiToken = _config.GetSection("APITokens").GetSection("GitHub").Value;
+
+            httpclient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("token", gitHubApiToken);
+            httpclient.DefaultRequestHeaders.Add("User-Agent", "C# Application");
+
+            HttpContent bodyContent = new StringContent("{\"title\":\"Testing\", \"body\":\"This is a test.\"}");
+
+            var callResponse = httpclient.PostAsync("https://api.github.com/repos/Villanite-Enterprises/VillaniteSite/issues", bodyContent).Result;
+
+            //var callResponse = httpclient.GetAsync("https://api.github.com/repos/Villanite-Enterprises/VillaniteSite/issues").Result;
+
+            ViewBag.Temp = callResponse.Content.ReadAsStringAsync().Result;
+
             return View();
         }
 
